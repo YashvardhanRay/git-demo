bb32d7498c30488473bd6ed6b2f8fba0ced2188f
YashvardhanRay <yashvardhan16200@gmail.com>
2025-02-15 16:11:31 +0530

    modifying user entity adding email

--- src/main/java/com/example/git/demo/model/User.java
+++ src/main/java/com/example/git/demo/model/User.java
@@ -1,4 +1,40 @@
 package com.example.git.demo.model;
 
 public class User {
+    private int id ;
+    private String userName;
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
