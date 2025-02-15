fc6a4b8a17ec1a4378406546eeb3e2edce218114
YashvardhanRay <yashvardhan16200@gmail.com>
2025-02-15 16:53:26 +0530

    modifing product entity adding model field

--- src/main/java/com/example/git/demo/model/Product.java
+++ src/main/java/com/example/git/demo/model/Product.java
@@ -1,4 +1,40 @@
 package com.example.git.demo.model;
 
 public class Product {
+    private int id;
+    private String productName;
+    private String model;
+    private String price;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
